a6420bb55422226974463c2db8422f0d2d185339
Johan Beimers <35890334+JohanOnPc@users.noreply.github.com>
2023-06-15 02:21:13 +0200

    Removed the cpp file of the queue to remove the linking errors with templates, by moving the implementation to the header file.

--- src/ThreadSafeQueue.h
+++ src/ThreadSafeQueue.h
@@ -10,12 +10,66 @@ class ThreadSafeQueue {
 public:
 	ThreadSafeQueue() {}
 
-	void Push(T value);
-	void WaitAndPop(T& value);
-	std::shared_ptr<T> WaitAndPop();
-	bool TryPop(T& value);
-	std::shared_ptr<T> TryPop();
-	bool IsEmpty() const;
+	inline void Push(T value)
+	{
+		std::lock_guard<std::mutex> lock(mut);
+		dataQueue.push(std::move(value));
+		dataConditional.notify_one();
+	}
+
+	inline void WaitAndPop(T& value)
+	{
+		std::unique_lock<std::mutex> lock(mut);
+		dataConditional.wait(lock, [this] {return !dataQueue.empty(); });
+		value = std::move(dataQueue.front());
+		dataQueue.pop();
+	}
+
+	inline std::shared_ptr<T> WaitAndPop()
+	{
+		std::unique_lock<std::mutex> lock(mut);
+		dataConditional.wait(lock, [this] {return !dataQueue.empty(); });
+		std::shared_ptr<T> result(
+			std::make_shared(std::move(dataQueue.front()))
+		);
+		dataQueue.pop();
+
+		return result;
+	}
+
+	inline bool TryPop(T& value)
+	{
+		std::lock_guard<std::mutex> lock(mut);
+		if (dataQueue.empty()) {
+			return false;
+		}
+
+		value = std::move(dataQueue.front());
+		dataQueue.pop();
+
+		return true;
+	}
+
+	inline std::shared_ptr<T> TryPop()
+	{
+		std::lock_guard<std::mutex> lock(mut);
+		if (dataQueue.empty()) {
+			return std::shared_ptr<T>();
+		}
+		std::shared_ptr<T> result(
+			std::make_shared<T>(std::move(dataQueue.front()))
+		);
+		dataQueue.pop();
+
+		return result;
+	}
+
+	inline bool IsEmpty() const
+	{
+		std::lock_guard<std::mutex> lock(mut);
+		return dataQueue.empty();
+	}
+
 
 private:
 	mutable std::mutex mut;
